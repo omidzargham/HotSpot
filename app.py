c63bc8221bb86a351eabe87a9b13e74a3cbb0f64
Benjamin Jong <twoten512@berkeley.edu>
2018-04-27 20:08:45 -0700

    Added category.html and routing function for it in app.py

--- app.py
+++ app.py
@@ -35,12 +35,17 @@ def add_book():
 @app.route("/book/<int:id>")
 def get_book(id):
 
-	con = lite.connect("books.db")
-	cur = con.cursor()
-	cur.execute("select id, title, author from books where id = " + str(id))
-	rows = cur.fetchall()
-
 	return render_template("viewbook.html", **locals())
+
+@app.route("/category")
+def get_category():
+
+	con = lite.connect("hotspot.db")
+	cur = con.cursor()
+	cur.execute("select max(ct), eid, cid, Category.Name from Category, (select att.ct as ct, Event.Event_id as eid, Event.Cat_id as cid from Event, (select count(*) as ct, Event_id from Attendance group by Event_id) as att where Event.Event_id = att.Event_id) as a where a.cid = Category.Cat_id group by a.cid order by Category.Name") 
+	rows = get_best_events_by_category()
+	print(rows)
+	return render_template("category.html", **locals())
 
 if __name__ == "__main__":
     app.run()
