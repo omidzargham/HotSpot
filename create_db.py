9f16345123e3ab0bbf4e65c19984ab175a306aac
Cindy Yuan <cyuan@berkeley.edu>
2018-05-02 15:12:59 -0700

    updated create_db

--- create_db.py
+++ create_db.py
@@ -1,6 +1,12 @@
 import sqlite3 as lite
 import sys
-import time
+import random
+
+cats = ['food','music','games','movies','artists','shop','holiday','academic','political']
+event = ['find hilfiger', '4/20', 'cute dogs day', 'rock concert', 'gamers unite', 'food for the poor', 'Cherry Blossom Festival', 'Music Busking', 'Alcatraz Island Tour', 'Geek Show 2: Bindlecon', 'Late Night Improv', 'SF Giants Baseball', 'Outside Lands', 'Wicked','The Lion King', 'American Musical Journey', 'Frozen', 'Hamilton', 'Art at the Met', 'Japanese Tea Garden']
+cities = ['berkeley', 'san francisco', 'oakland', 'los angeles', 'las vegas']
+state = ['ca']
+
 
 con = lite.connect("hotspot.db")
 
@@ -13,8 +19,41 @@ with con:
     cur.execute("CREATE TABLE IF NOT EXISTS Attendance (Att_id INTEGER PRIMARY KEY, Participant_id INTEGER, Event_id INTEGER)")
     cur.execute("CREATE TABLE IF NOT EXISTS Category (Cat_id INTEGER PRIMARY KEY, Name TEXT)")
 
+    for i in range(0,len(cats)):
+        print(i)
+        cur.execute("INSERT INTO Category (Cat_id, Name) VALUES ("+str(i)+",\'"+cats[i]+"\')") #{}, {},{},{},{},{},{},{})".format(eid,ename,did,stime,etime,descrip,cid,cost))
+        
+
+    for i in range(0,len(cities)):
+        did = i
+        dname = cities[i]
+        state = 'ca'
+        city = cities[i]
+        cur.execute("INSERT INTO Destination (Dest_id, Dest_name, State, City) VALUES ("+str(did)+",\'"+dname+"\',\'"+state+"\',\'"+city+"\')") #{}, {},{},{},{},{},{},{})".format(eid,ename,did,stime,etime,descrip,cid,cost))
+            
+
+    aid = 0
+    for i in range(0,len(event)):
+        eid = i
+        ename = event[i]
+        did = random.randint(0,len(city)-1)
+        stime = '2018-01-01 1:23'
+        etime = '2018-01-01 1:23'
+        descrip = 'description'
+        cid = random.randint(0,len(cats)-1)
+        cost = 1.0
+        participants = [k for k in range(0,100)]
+        for j in range(0,random.randint(0,100)):
+            r = random.randint(0,len(participants) - 1)
+            pid = participants[r]
+
+            participants.remove(pid)
+            cur.execute("INSERT INTO Attendance (Att_id, Participant_id, Event_id) VALUES ("+str(aid)+","+str(pid)+","+str(eid)+")") #{}, {},{},{},{},{},{},{})".format(eid,ename,did,stime,etime,descrip,cid,cost))
+            aid+=1
+        #cur.execute("INSERT INTO Event (Event_id, Event_name, Dest_id, Start_time,End_time, Description, Cat_id, Cost) VALUES ({}, {},{},{},{},{},{},{})".format(eid,ename,did,stime,etime,descrip,cid,cost))
+        cur.execute("INSERT INTO Event (Event_id, Event_name, Dest_id, Start_time,End_time, Description, Cat_id, Cost) VALUES ("+str(eid)+",\'"+ename+"\',"+str(did)+",\'"+stime+"\',\'"+etime+"\',\'"+descrip+"\',"+str(cid)+","+str(cost)+")")#{}, {},{},{},{},{},{},{})".format(eid,ename,did,stime,etime,descrip,cid,cost))
     
-    
+
     cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (1, 'Andrea', 21, 'Female')")
     cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (2, 'Cindy', 21, 'Female')")
     cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (3, 'Ben', 21, 'Male')")
@@ -23,39 +62,30 @@ with con:
     cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (6, 'Mary', 24, 'Female')")
     cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (7, 'Jeffrey', 25, 'Male')")
     cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (8, 'Sam', 28, 'Male')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (9, 'George', 28, 'Male')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (10, 'Paul', 38, 'Male')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (11, 'Karen', 37, 'Female')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (12, 'Jim', 28, 'Male')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (13, 'Lilly', 22, 'Female')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (14, 'Hannah', 28, 'Female')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (15, 'Lauren', 26, 'Female')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (16, 'Jonathan', 21, 'Male')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (17, 'Kristen', 22, 'Female')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (18, 'Nathan', 25, 'Male')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (19, 'Luke', 31, 'Male')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (20, 'Shelly', 22, 'Female')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (21, 'Kelly', 37, 'Female')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (22, 'James', 28, 'Male')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (23, 'Laila', 22, 'Female')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (24, 'Harriet', 28, 'Female')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (25, 'Lucy', 26, 'Female')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (26, 'Jared', 21, 'Male')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (27, 'Barbara', 42, 'Female')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (28, 'Max', 25, 'Male')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (29, 'Larry', 31, 'Male')")
+    cur.execute("INSERT INTO Participant (Participant_id, Name, Age, Gender) VALUES (30, 'Shirley', 32, 'Female')")
 
-    cur.execute("INSERT INTO Destination (Dest_id, Dest_name, State, City) VALUES (1, 'Union Square', 'California', 'San Francisco')")
-    cur.execute("INSERT INTO Destination (Dest_id, Dest_name, State, City) VALUES (2, 'Alcatraz Island', 'California', 'San Francisco')")
-    cur.execute("INSERT INTO Destination (Dest_id, Dest_name, State, City) VALUES (3, 'Bindlestiff Studio', 'California', 'San Francisco')")
-    cur.execute("INSERT INTO Destination (Dest_id, Dest_name, State, City) VALUES (4, 'Shelton Theater', 'California', 'San Francisco')")
-    cur.execute("INSERT INTO Destination (Dest_id, Dest_name, State, City) VALUES (5, 'AT&T Park', 'California', 'San Francisco')")
-
-
-    cur.execute("INSERT INTO Event (Event_id, Event_name, Dest_id, Start_time, End_time, Description, Cat_id, Cost) VALUES (1, 'Cherry Blossom Festival', 1, null, null, 'Come see the beautiful cherry blossoms!', 0, 0.0)")
-    cur.execute("INSERT INTO Event (Event_id, Event_name, Dest_id, Start_time, End_time, Description, Cat_id, Cost) VALUES (2, 'Music Busking', 1, null, null, 'Listen to a variety of musicians play on the streets of SF.', 0, 0.0)")
-    cur.execute("INSERT INTO Event (Event_id, Event_name, Dest_id, Start_time, End_time, Description, Cat_id, Cost) VALUES (3, 'Alcatraz Island Tour', 2, null, null, 'Tour the old prison of Alcatraz', 0, 0.0)")
-    cur.execute("INSERT INTO Event (Event_id, Event_name, Dest_id, Start_time, End_time, Description, Cat_id, Cost) VALUES (4, 'Geek Show 2: Bindlecon', 3, null, null, 'Movie event', 0, 0.0)")
-    cur.execute("INSERT INTO Event (Event_id, Event_name, Dest_id, Start_time, End_time, Description, Cat_id, Cost) VALUES (5, 'Late Night Improv', 4, null, null, 'A fun night of comedy', 0, 0.0)")
-    cur.execute("INSERT INTO Event (Event_id, Event_name, Dest_id, Start_time, End_time, Description, Cat_id, Cost) VALUES (6, 'SF Giants Baseball', 5, null, null, 'Baseball yay', 0, 0.0)")
-
-    cur.execute("INSERT INTO Attendance (Att_id, Participant_id, Event_id) VALUES (1, 1, 3)")
-    cur.execute("INSERT INTO Attendance (Att_id, Participant_id, Event_id) VALUES (2, 2, 3)")
-    cur.execute("INSERT INTO Attendance (Att_id, Participant_id, Event_id) VALUES (3, 3, 3)")
-    cur.execute("INSERT INTO Attendance (Att_id, Participant_id, Event_id) VALUES (4, 4, 3)")
-    cur.execute("INSERT INTO Attendance (Att_id, Participant_id, Event_id) VALUES (5, 5, 3)")
-    cur.execute("INSERT INTO Attendance (Att_id, Participant_id, Event_id) VALUES (6, 6, 3)")
-    cur.execute("INSERT INTO Attendance (Att_id, Participant_id, Event_id) VALUES (7, 7, 3)")
-    cur.execute("INSERT INTO Attendance (Att_id, Participant_id, Event_id) VALUES (8, 8, 1)")
-    cur.execute("INSERT INTO Attendance (Att_id, Participant_id, Event_id) VALUES (9, 1, 2)")
-    cur.execute("INSERT INTO Attendance (Att_id, Participant_id, Event_id) VALUES (10, 5, 2)")
-    cur.execute("INSERT INTO Attendance (Att_id, Participant_id, Event_id) VALUES (11, 1, 4)")
-    cur.execute("INSERT INTO Attendance (Att_id, Participant_id, Event_id) VALUES (12, 2, 4)")
-    cur.execute("INSERT INTO Attendance (Att_id, Participant_id, Event_id) VALUES (13, 1, 2)")
-
-
-
-    '''
-    cur.execute("INSERT INTO Books (title, author) VALUES ('Info Org', 'Bob')")
-    cur.execute("INSERT INTO Books (title, author) VALUES ('Info Law', 'Deidre')")
-    cur.execute("INSERT INTO Books (title, author) VALUES ('Math', 'Julie')")
-    '''
+    #cur.execute("INSERT INTO Event (Event_id, Event_name, Dest_id, Start_time,End_time, Description, Cat_id, Cost) VALUES (0,'hey',0,null,null,'des',0,0)")#{}, {},{},{},{},{},{},{})".format(eid,ename,did,stime,etime,descrip,cid,cost))
+    
+    #cur.execute("INSERT INTO Event (Event_id, Event_name, Dest_id, Start_time,End_time, Description, Cat_id, Cost) VALUES ("+str(eid)+","+ename+","+str(did)+",null,null,"+descrip+","+str(cid)+","+str(cost)+")")#0,'hey',0,null,null,'des',0,0)")#{}, {},{},{},{},{},{},{})".format(eid,ename,did,stime,etime,descrip,cid,cost))
+    
